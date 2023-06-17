081404ce924a54d6a1094c0f2d24354287d89778
Omikunle Timileyin Micheal <Omikunletimilehin@gmail.com>
2023-06-17 23:46:43 +0100

    changes in the dark mode and get gates js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,13 +1,20 @@
-// getDates.js
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the current year
-    var currentYear = new Date().getFullYear();
-    var yearSpan = document.getElementById("year");
-    yearSpan.textContent = currentYear;
-  
-    // Get the last modified date
-    var lastModified = document.lastModified;
-    var lastModifiedSpan = document.getElementById("lastModified");
-    lastModifiedSpan.textContent = lastModified;
-  });
+// JavaScript for displaying the last modification date
+const lastModifiedSpan = document.getElementById('lastModified');
+const lastModified = new Date(document.lastModified);
+lastModifiedSpan.textContent = lastModified.toDateString();
+
+// JavaScript for the hamburger button functionality
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const menuItems = document.querySelector('.menu-items');
+
+// Toggle the visibility of menu items on clicking the hamburger button
+hamburgerBtn.addEventListener('click', () => {
+  menuItems.classList.toggle('show-menu');
+});
+
+// JavaScript function to toggle dark mode
+function toggleDarkMode() {
+  const mainElement = document.querySelector('main');
+  mainElement.classList.toggle('dark-mode');
+}
   
