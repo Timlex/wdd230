f865a6be8ccd4572e64d9cf84a79c40262b10073
Omikunle Timileyin Micheal <Omikunletimilehin@gmail.com>
2023-07-05 14:19:27 +0100

    adding weather html to home page

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7493783231072&lon=6.634338593584977&units=imperial&appid=cdef6a837b4a6e9f4028eaf963b5e847';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&units=imperial&appid=cdef6a837b4a6e9f4028eaf963b5e847';
 
 async function apiFetch() {
   try {
@@ -19,15 +19,14 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-  const temperature = Math.round(data.main.temp);
-  const icon = data.weather[0].icon;
-  const description = data.weather[0].description;
-
-  currentTemp.innerHTML = `${temperature}&deg;F`;
-  const iconSrc = `https://openweathermap.org/img/w/${icon}.png`;
-  weatherIcon.setAttribute('src', iconSrc);
-  weatherIcon.setAttribute('alt', description);
-  captionDesc.textContent = description;
-}
-
-apiFetch();
+    const temperature = Math.round(data.main.temp);
+    const icon = data.weather[0].icon;
+    const description = data.weather[0].description;
+  
+    currentTemp.innerHTML = `${temperature}&deg;F`;
+    const iconSrc = `https://openweathermap.org/img/wn/${icon}10d@2x.png`;
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', description);
+    captionDesc.textContent = description;
+  }
+  
